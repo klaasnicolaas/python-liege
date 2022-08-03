e28fb0be14feef01eb4546c40c3b99612b3cb249
Klaas Schoute <klaas_schoute@hotmail.com>
2022-08-03 23:46:30 +0000

    Change value for spot_id because not unique

--- test_output.py
+++ test_output.py
@@ -11,13 +11,22 @@ async def main() -> None:
     async with ODPLiege() as client:
         garages = await client.garages(limit=12)
         disabled_parkings = await client.disabled_parkings(limit=5)
-        print(disabled_parkings)
+        print(garages)
 
         count: int
-        for index, item in enumerate(garages, 1):
+        for index, item in enumerate(disabled_parkings, 1):
             count = index
             print(item)
-        print(f"{count} locations found")
+
+        # Count unique id's in disabled_parkings
+        unique_values: list[str] = []
+        for item in disabled_parkings:
+            unique_values.append(item.spot_id)
+        num_values = len(set(unique_values))
+
+        print("__________________________")
+        print(f"Total locations found: {count}")
+        print(f"Unique ID values: {num_values}")
 
 
 if __name__ == "__main__":
